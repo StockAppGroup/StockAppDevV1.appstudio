1dee847448fa3df25fb904fd0bd5c09558ac11bc
Tanner <tannerbutler15@gmail.com>
2019-11-18 13:07:16 -0600

    API

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,18 +1,22 @@
-
+let apiKey = "pk_9a389ccddf364770a78c9a3ce53c00e2"
 
 btnSubmitStock.onclick=function(){
-    let myurl = "https://cors-anywhere.herokuapp.com/https://cloud.iexapis.com/stable/tops?token=pk_9a389ccddf364770a78c9a3ce53c00e2&symbols=" + inptStock.value;
+    let myurl = "https://cors-anywhere.herokuapp.com/https://cloud.iexapis.com/stable/stock/"+inptStock.value+"/company?token="+apiKey;
 
     $.ajax({
             url: myurl,
             //headers: {
-            //'Authorization':'Bearer pk_9a389ccddf364770a78c9a3ce53c00e2',},
+            //'Authorization':'Bearer sk_59c02c88a7614d68b2d11f548311b6f9',},
             method: 'GET',
             dataType: 'json',
-            success: function(data){   // this function runs with the returned data if trip is successful
-               
-            console.log(data[0].symbol)
+            success: function(data){ // this function runs with the returned data if trip is successful
+            let totalresults = data.total
+            if (totalresults > 0){
+            console.log(data)
             }
+            else 
+              console.log("No businesses found")
+              }
          }); 
 
 }
