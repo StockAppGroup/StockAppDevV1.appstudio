d6c3e4ad54626db2b3bfcf7cce54ef5abe36ae91
bseagy <bse29687@creighton.edu>
2019-11-18 15:10:57 -0600

    Update Login.js

--- forms/Login/Login.js
+++ forms/Login/Login.js
@@ -1,4 +1,4 @@
-
+//User login process
 btnRegister.onclick=function(){
   ChangeForm(Register)
 }
@@ -10,7 +10,7 @@ btnLogin.onclick=function(){
     req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=bse29687&pass=BIA375pass&database=375groupa8&query=" + query) = ""    
     results = JSON.parse(req1)
     if (req1.status == 200) { //everything worked.
-        if (results.length = 0) {
+        if (results.length = 0) { //there is no user or the password was entered incorrectly
             lblResult.value = "Incorrect username or password"
           } else {
               ChangeForm(Home)
