43be1be22be2991164f21de58d712dd684b8d472
Tanner <tannerbutler15@gmail.com>
2019-11-18 14:32:38 -0600

    I think I figured this api out

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,7 +1,8 @@
 let apiKey = "pk_9a389ccddf364770a78c9a3ce53c00e2"
+let apiKey1 = "sk_59c02c88a7614d68b2d11f548311b6f9"
 /*
 btnSubmitStock.onclick=function(){
-    let myurl = "https://cors-anywhere.herokuapp.com/https://cloud.iexapis.com/stable/stock/"+inptStock.value+"/company?token="+apiKey;
+    let myurl = "https://cors-anywhere.herokuapp.com/https://sandbox.iexapis.com/stable/stock/"+inptStock.value+"/company?token="+apiKey;
 
     $.ajax({
             url: myurl,
@@ -21,8 +22,10 @@ btnSubmitStock.onclick=function(){
 
 }
 */
+
 btnSubmitStock.onclick=function(){
-    let myurl = "https://cors-anywhere.herokuapp.com/https://cloud.iexapis.com/stable/tops?token="+ apiKey+"&symbols=" + inptStock.value;
+    // let myurl = "https://cors-anywhere.herokuapp.com/https://cloud.iexapis.com/stable/tops?token="+ apiKey+"&symbols=" + inptStock.value;
+    let myurl = "https://cors-anywhere.herokuapp.com/https://cloud.iexapis.com/stable/stock/"+inptStock.value+"/company?token="+apiKey;
 
     $.ajax({
             url: myurl,
@@ -32,8 +35,10 @@ btnSubmitStock.onclick=function(){
             dataType: 'json',
             success: function(data){   // this function runs with the returned data if trip is successful
 
-            console.log(data)
+            console.log(data.state)
             }
          });
 
 }
+
+// GET /stock/{symbol}/logo
