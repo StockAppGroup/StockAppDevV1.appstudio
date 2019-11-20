b221efb39698804c6d2416a4455d34982cef3e49
Tanner <tannerbutler15@gmail.com>
2019-11-20 14:50:11 -0600

    UPDATING

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,27 +1,6 @@
 let apiKey = "pk_9a389ccddf364770a78c9a3ce53c00e2"
 let apiKey1 = "sk_59c02c88a7614d68b2d11f548311b6f9"
-/*
-btnSubmitStock.onclick=function(){
-    let myurl = "https://cors-anywhere.herokuapp.com/https://sandbox.iexapis.com/stable/stock/"+inptStock.value+"/company?token="+apiKey;
 
-    $.ajax({
-            url: myurl,
-            //headers: {
-            //'Authorization':'Bearer sk_59c02c88a7614d68b2d11f548311b6f9',},
-            method: 'GET',
-            dataType: 'json',
-            success: function(data){ // this function runs with the returned data if trip is successful
-            let totalresults = data
-            if (totalresults > 0){
-            console.log(data)
-            }
-            else 
-              console.log("No businesses found")
-              }
-         }); 
-
-}
-*/
 
 btnSubmitStock.onclick=function(){
     // let myurl = "https://cors-anywhere.herokuapp.com/https://cloud.iexapis.com/stable/tops?token="+ apiKey+"&symbols=" + inptStock.value;
@@ -29,8 +8,6 @@ btnSubmitStock.onclick=function(){
 
     $.ajax({
             url: myurl,
-            //headers: {
-            //'Authorization':'Bearer pk_9a389ccddf364770a78c9a3ce53c00e2',},
             method: 'GET',
             dataType: 'json',
             success: function(data){   // this function runs with the returned data if trip is successful
