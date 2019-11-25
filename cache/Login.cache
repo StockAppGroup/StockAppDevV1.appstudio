dc83ba8f0d4755cd586c3e5b39c48fe0d1d2d81d
bseagy <bse29687@creighton.edu>
2019-11-24 21:07:20 -0600

    Login_Final
    Login page is completed

--- cache/Login.cache
+++ cache/Login.cache
@@ -3,14 +3,16 @@ btnRegister.onclick=function(){
   ChangeForm(Register)
 }
 
+
+
 btnLogin.onclick=function(){
     let checkPass = inptPassword.value
     let checkUser = inptUsername.value
-    let query = "SELECT user_name, password FROM user WHERE user_name = " + ' " ' + checkUser + ' " ' + " AND password = " + ' " ' + checkPass + ' " '
-    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=bse29687&pass=BIA375pass&database=375groupa8&query=" + query) = ""    
-    results = JSON.parse(req1)
+    let query = "SELECT user_name, password FROM user WHERE user_name = " + '"' + checkUser + '"' + "AND password = " + '"' + checkPass +  '"'
+    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=bse29687&pass=BIA375pass&database=375groupa8&query=" +  query)  
     if (req1.status == 200) { //everything worked.
-        if (results.length = 0) { //there is no user or the password was entered incorrectly
+        results = JSON.parse(req1.responseText)
+        if (results.length == 0) { //there is no user or the password was entered incorrectly
             lblResult.value = "Incorrect username or password"
           } else {
               ChangeForm(Home)
@@ -20,6 +22,4 @@ btnLogin.onclick=function(){
         lblResult.style.display = "block"  // none to hide
         lblResult.value = "Error Connection Not Made: " + req1.status + " readystate " + req1.readyState + " status text " + req1.statusText;
     }
-   
-    
 }
--- forms/Register/Register.js
+++ forms/Register/Register.js
@@ -1,4 +1,5 @@
 //User registration process
+/*
 btnSignUp.onclick=function(){
   let newUser = inptSignUpUser.value
   let newPass = inptSignUpPass.value
@@ -7,33 +8,35 @@ btnSignUp.onclick=function(){
   let queryUserExist = "SELECT user_name FROM user WHERE user_name = " + ' " ' + newUser + ' " '
   let queryPhoneExist = "SELECT phone_number FROM user WHERE phone_number = " + ' " ' + newPhone + ' " '
   let queryNew = "INSERT INTO user (user_name,password,phone_number) VALUES (' " + newUser + " ', ' " + newPass + " ', ' " + newPhone + " ') "
-       req2 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=bse29687&pass=BIA375pass&database=375groupa8&query=" + queryUserExist) = ""    
-          results1 = JSON.parse(req2)
+       req2 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=bse29687&pass=BIA375pass&database=375groupa8&query=" + queryUserExist)   
           if (req2.status == 200) { //everything worked.
+              results1 = JSON.parse(req2.responseText)
               if results1.length > 0 {
-              lblVerified.value = "Please choose another username"
+                lblVerified.value = "Please choose another username"
               } else {
                     if (newPass !== verifyPass) {
                     lblVerified.value = "Verified password needs to match"
                     } else {
-                                 req3 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=bse29687&pass=BIA375pass&database=375groupa8&query=" + queryPhoneExist) = ""    
+                                 req3 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=bse29687&pass=BIA375pass&database=375groupa8&query=" + queryPhoneExist)   
                                       results2 = JSON.parse(req3)
                                       if (req3.status == 200) { //everything worked.
                                         if results2.lenght > 0 {
                                         lblVerified.value = "A user is already registered under this phone number"
                                         } else {
-                                              req4 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=bse29687&pass=BIA375pass&database=375groupa8&query=" + queryNew) = ""    
+                                              req4 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=bse29687&pass=BIA375pass&database=375groupa8&query=" + queryNew)  
                                                         if (req4.status == 200) { //everything worked.
                                                         ChangeForm(Home)
-                                                        } else {
+                                                          } else {
                                                             lblVerified.style.display = "block"  // none to hide
                                                             lblVerified.value = "Error Connection Not Made: " + req4.status + " readystate " + req4.readyState + " status text " + req4.statusText;
-                                                            }
+                                            } else {
                                               lblVerified.style.display = "block"  // none to hide
                                               lblVerified.value = "Error Connection Not Made: " + req3.status + " readystate " + req3.readyState + " status text " + req3.statusText;
-                                      }
+                                              }
+                          } else {
                             lblVerified.style.display = "block"  // none to hide
                             lblVerified.value = "Error Connection Not Made: " + req2.status + " readystate " + req2.readyState + " status text " + req2.statusText;
+                            }
                       }
                 } 
           }
@@ -47,9 +50,9 @@ btnSignUp.onclick=function(){
       7) else: insert the data using queryNew (req4 = Ajax)X
       8) ChangeForm(Home)X
       9) Tie everything off with error messagesX
- */     
-}
 
+}
+*/
 
 btnBack.onclick=function(){
   ChangeForm(Login)
